4ee4e41cddeb413a2bdcde5315b67cbebdfeaeba
Federico Rampazzo <federamp91@gmail.com>
2021-03-13 19:25:14 -0300

    Initial Code

---
+++ Repaso11/Repaso11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repaso11
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
