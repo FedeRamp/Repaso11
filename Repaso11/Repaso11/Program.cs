78b7ce8b64ffd13dda592e61bdec0ad88d11eddb
Federico Rampazzo <federamp91@gmail.com>
2021-03-14 11:08:00 -0300

    Abandono, seguimos otro día
    La idea era crear un txt con una lista medianamente completa de nombres, chequee si alguna de esas palabras está ahí, si no, es un apellido. Recordar averiguar cómo escanear las lineas de un txt, qué pasa si el nombre no está en la lista? el programa diría que son dos apellidos?

--- Repaso11/Repaso11/Program.cs
+++ Repaso11/Repaso11/Program.cs
@@ -4,9 +4,38 @@ namespace Repaso11
 {
     class MainClass
     {
+        //Identificar nombre, apellido, edad
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string ingreso = "";
+            do
+            {
+                Console.WriteLine("Ingresar una palabra");
+                ingreso = Console.ReadLine();
+                if (ingreso == "") Console.WriteLine("Algo, no vacío");
+            } while (ingreso == "");
+
+            string[] palabras = ingreso.Split(' ');
+            int indexEdad = -1;
+            int indexNombre = -1;
+            int indexApellido = -1;
+            if (palabras.Length == 3)
+            {
+                for(int i = 0; i < 3; i++)
+                {
+                    if(int.TryParse(palabras[i], out int x))
+                    {
+                        indexEdad = i;
+                        break;
+                    }
+                }
+                if (indexEdad == -1) Console.WriteLine("No se encontró una edad");
+                for  
+            } else
+            {
+                Console.WriteLine("La próxima ingresar 3 palabras por favor");
+            }
+
         }
     }
 }
